9adf6992deaad5e7ad6a303e9f300847b88242db
Aman Bansal <amanbansal@github.com>
2018-04-02 07:17:47 +0530

    updated functionalities are working

--- ecommerce.js
+++ ecommerce.js
@@ -4,7 +4,10 @@ var $modal = $('#editModal');
 ecom.subTotal = 0;
 ecom.checkout = 0;
 ecom.discount = 0;
+ecom.totalQuantity = 0;
 ecom.t;
+ecom.price;
+ecom.promo = 0;
 ecom.database = firebase.database().ref('productsInCart');
 (function (ecom) {
     ecom.ecomlist = function (snapshot) {
@@ -23,28 +26,53 @@ ecom.database = firebase.database().ref('productsInCart');
                 $itemList.find('.styles').html(snapshot.val()[i].p_style);
                 $itemList.find('.sizes').html(snapshot.val()[i].p_selected_size.code);
                 $itemList.find('.quan').html(snapshot.val()[i].p_quantity);
-                $itemList.find('.prices').html(snapshot.val()[i].p_price);
-                ecom.subTotal = ecom.subTotal + snapshot.val()[i].p_price;                
+                $itemList.find('.prices').html(snapshot.val()[i].p_price * snapshot.val()[i].p_quantity);
+                ecom.price = snapshot.val()[i].p_price * snapshot.val()[i].p_quantity;
+                ecom.subTotal = ecom.subTotal + ecom.price;
+                ecom.totalQuantity = ecom.totalQuantity +  parseInt(snapshot.val()[i].p_quantity);
                 $itemList.find('#edit').on('click', function (e) {
                     e.preventDefault();
                     $('.modal-image').attr('src', snapshot.val()[i].p_image);
                     $('.modal-qual').html(snapshot.val()[i].p_name);
-                    $('.modal-price').html(snapshot.val()[i].p_price);
+                    $('.modal-price').html(snapshot.val()[i].p_price * snapshot.val()[i].p_quantity);
+                    $('.sizedrp option').each(function () {
+                        if ($(this).val() == snapshot.val()[i].p_selected_size.code)
+                            $(this).attr('selected', true);
+                    });
+                    $('.qtyDrp option').each(function () {
+                        if ($(this).val() == snapshot.val()[i].p_quantity)
+                            $(this).attr('selected', true);
+                    });
+                    snapshot.val()[i].p_available_options.colors.forEach(function (color) {
+                        var $label = $('<label>').prop('for', color.name);
+                        var $input = $('<input type = "radio">').prop('id', color.name).prop('name', 'colors').prop('value', color.name).addClass('radio-buttons');
+                        $input.css('background-color', color.hexcode);
+                        if (color.name == snapshot.val()[i].p_selected_color.name) {
+                            $input.prop('checked', true);
+                        }
+                        $label.appendTo($('#editModal').find('.color-box'));
+                        $input.appendTo($('#editModal').find('.color-box'));
+                    });
                     $modal.modal('show');
                     ecom.t = i;
                 })
-
-                $('.sizedrp')
                 
                 $('.edit-modal-btn').on('click',function(e){
                     if(ecom.t === i){
-                        console.log(i);
                         firebase.database().ref('productsInCart/' + i).update({
                             p_quantity:  $('.qtyDrp option:selected').val(),
+                        });
+                        if($('.sizedrp option:selected').val() === 'Size'){
+                            alert('Please enter a valid size')
+                            return false;
+                        }
+                        firebase.database().ref('productsInCart/' + i + '/p_selected_color/').update({
+                            name: $('input[type=radio]:checked').val(),
                         });
                         firebase.database().ref('productsInCart/' + i + '/p_selected_size/').update({
                             code:  $('.sizedrp option:selected').val(),
                         });
+                        $('editModal').find('.modal-color-options').html('');
                     }
                 })
                                
@@ -56,20 +84,19 @@ ecom.database = firebase.database().ref('productsInCart');
     ecom.ecomlist();
 
     ecom.discountLogic = function() {
-        if(ecom.datalen.length>10){
+        console.log(ecom.totalQuantity)
+        if(ecom.totalQuantity>7){
             ecom.checkout = ecom.subTotal * 0.75;
-            //consoleconsole.log(ecom.checkout);
         }
-        else if(ecom.datalen.length>3 && ecom.datalen.length<=6){
+        else if(ecom.totalQuantity>3 && ecom.totalQuantity<=6){
             ecom.checkout = ecom.subTotal * 0.9;
-            //console.log(ecom.checkout);
         }
-        else if(ecom.datalen.length === 3){
+        else if(ecom.totalQuantity === 3){
             ecom.checkout = ecom.subTotal * 0.95;
         }
         else{
             ecom.checkout = ecom.subTotal * 1;
         }
-        $('.est-total').html('$' + ' ' + ecom.checkout);
+        $('.est-total').html('$' + ' ' + ecom.checkout);          
     }
 })(ecom);
